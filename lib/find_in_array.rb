7b67bb31d18d3311f64b0326397d09409a454e6d
Madeline Rose Stalter <madelinerose16@outlook.com>
2020-07-28 17:29:16 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,11 +1,9 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
-  length = array.length
-  
-  length.times do |index|
+  array = array.length # know how many items in an array
+  value_to_find = index
+   length.times do |index|
     puts array|index|
 end
 
 
 
-#index[n]
