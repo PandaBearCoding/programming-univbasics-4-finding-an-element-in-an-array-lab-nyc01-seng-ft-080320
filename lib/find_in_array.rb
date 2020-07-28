5ee1b096d26d1fd46c9ad094d251fc7e68f5361e
Madeline Rose Stalter <madelinerose16@outlook.com>
2020-07-28 17:34:50 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,17 +1,10 @@
+# The code below allows us to loop through an array using the while loop and .length (to shuffle/iterate through each element)
+
 def find_element_index(array, value_to_find)
-  count = 0
-  while count < array.length do
-    
-  count += 1
-end
-end
-
-
-
-value_to_find)
   count = 0
   while count < array.length do
     puts array[count] == value_to_find
     count += 1
   end
 end
+
